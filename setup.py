65a7596064a80c1c86e01c2285efa95985da9841
Mark Fiers <Mark.Fiers@cme.vib-kuleuven.be>
2015-08-27 09:37:25 +0200

    version bump

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ class Tox(TestCommand):
 DESCRIPTION = "Yaml based application configuration"
 
 setup(name='fantail',
-      version='0.2',
+      version='0.2.1',
       description=DESCRIPTION,
       author='Mark Fiers',
       author_email='mark.fiers42@gmail.com',
